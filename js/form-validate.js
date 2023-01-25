8b467e31f9de733f8ea401e21fd15e8fe04e2f24
TudorMagirescu <tudor.magirescu@gmail.com>
2023-01-25 15:53:26 +0100

    Added basic validation information

--- js/form-validate.js
+++ js/form-validate.js
@@ -17,27 +17,137 @@ document.getElementById("sign-up-button").onclick = function(event) {
 };
 
 function validate(details, property) {
-    showValidationInformation(property, "Looks Good!", true);
-    return true;
+
+    switch(property) {
+
+        case "username":
+
+            if(details[property] === '') {
+                showValidationInformation(property, "Field is required!", false);
+                return false;
+            }
+
+            if(details[property].length < 5) {
+                console.log("here");
+                showValidationInformation(property, "Field must have length at least 5!", false);
+                return false;
+            }
+
+            if(details[property].length > 12) {
+                showValidationInformation(property, "Field must have length at most 12!", false);
+                return false;
+            }
+
+            //additional checks with regex
+
+            showValidationInformation(property, "Looks good!", true);
+            return true;
+
+        case "password":
+
+            if(details[property] === '') {
+                showValidationInformation(property, "Field is required!", false);
+                return false;
+            }
+
+            if(details[property].length < 12) {
+                showValidationInformation(property, "Field must have length at least 12!", false);
+                return false;
+            }
+
+            //additional checks with regex
+
+            showValidationInformation(property, "Looks good!", true);
+            return true;
+
+        case "name":
+
+            if(details[property] === '') {
+                showValidationInformation(property, "Field is required!", false);
+                return false;
+            }
+
+            //additional checks with regex
+
+            showValidationInformation(property, "Looks good!", true);
+            return true;
+
+        case "address":
+        case "bio":
+
+            showValidationInformation(property, "Looks good!", true);
+            return true;
+
+        case "country":
+        case "gender":
+        case "language":
+
+            if(details[property] === '') {
+                showValidationInformation(property, "Field is required!", false);
+                return false;
+            }
+
+            showValidationInformation(property, "Looks good!", true);
+            return true;
+
+        case "zip":
+
+            if(details[property] === '') {
+                showValidationInformation(property, "Field is required!", false);
+                return false;
+            }
+
+            //additional checks with regex
+
+            showValidationInformation(property, "Looks good!", true);
+            return true;
+
+        case "email":
+
+            if(details[property] === '') {
+                showValidationInformation(property, "Field is required!", false);
+                return false;
+            }
+
+            //additional checks with regex
+
+            showValidationInformation(property, "Looks good!", true);
+            return true; 
+
+        default:
+            showValidationInformation(property, "Does not Look Good!", false);
+            return false;
+    }
 }
 
 function showValidationInformation(property, text, isValid) {
 
     let pElement = document.createElement("p");
     let signUpDiv = document.getElementById("sign-up-" + property + "-div");
-  
+
     if(signUpDiv.lastChild.tagName === 'P')
         signUpDiv.removeChild(signUpDiv.lastChild);
+    
+    let crossTickSpan = document.getElementById(property + "-cross-tick");
+    crossTickSpan.innerHTML = '';
 
-    pElement.style.fontSize = "16px";
+    pElement.style.fontSize = "13px";
     if(isValid) {
         pElement.style.color = "green";
         document.getElementById("sign-up-" + property + "-input").style.borderColor = "green";
+
+        crossTickSpan.style.fontSize = "12px";
+        crossTickSpan.style.color = "rgb(0, 180, 0)";
+        crossTickSpan.appendChild(document.createTextNode("✔"));
     }
         
     else {
         pElement.style.color = "red";
         document.getElementById("sign-up-" + property + "-input").style.borderColor = "red";
+
+        crossTickSpan.style.fontSize = "12px";
+        crossTickSpan.style.color = "red";
+        crossTickSpan.appendChild(document.createTextNode("✖"));
     }
 
     pElement.appendChild(document.createTextNode(text));
